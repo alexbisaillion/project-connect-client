effe5309d489baadba486cc474cc7c33a957672d
Alex Bisaillion <a.bisaillion@gmail.com>
2020-10-03 09:05:16 -0400

    Add base url env variable

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosResponse } from "axios"
 axios.defaults.withCredentials = true;
 
-const baseUrl: string = "http://localhost:4000"
+const baseUrl: string = process.env.BASE_URL || "http://localhost:4000"
 
 export const getUsers = async (): Promise<AxiosResponse<IUser[]>> => {
   try {
